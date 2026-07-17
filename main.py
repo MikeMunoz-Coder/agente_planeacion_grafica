ae61cc4b07795cce20d1486ba398cccb0fb18fea
mike_munoz <mikesmithe7@gmail.com>
2026-07-17 09:14:49 -0500

    feat: integra analisis de documento PDF

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ agente = AgenteOrquestador()
 respuesta = agente.ejecutar(
     #"¿Cuál es la información de la OP-1050?"
     #"¿Cuál es la última orden de producción realizada para el cliente JGB?"
-    "Busca trabajos similares relacionados con stickers adhesivos"
-    # """
-    # Analiza el documento PDF:
-    # documentos/orden_produccion_OP1050.pdf
+    # "Busca trabajos similares relacionados con stickers adhesivos"
+    """
+    Analiza el documento PDF:
+    documentos/orden_produccion_OP1050.pdf
 
-    # Extrae la información de la orden de producción.
-    # """
+    Extrae la información de la orden de producción.
+    """
 )
 
 
