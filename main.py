5b808bb3416b211e42f5be34c9e3e4d25a8fa2df
mike_munoz <mikesmithe7@gmail.com>
2026-07-17 09:35:27 -0500

    feat: agente planeacion grafica con herramientas funcionales

--- main.py
+++ main.py
@@ -5,14 +5,16 @@ agente = AgenteOrquestador()
 
 
 respuesta = agente.ejecutar(
-    #"¿Cuál es la información de la OP-1050?"
+    "¿Cuál es la información de la OP-1050?"
     #"¿Cuál es la última orden de producción realizada para el cliente JGB?"
     # "Busca trabajos similares relacionados con stickers adhesivos"
     """
     Analiza el documento PDF:
     documentos/orden_produccion_OP1050.pdf
 
-    Extrae la información de la orden de producción.
+    Extrae los datos más importantes:
+    número de OP, cliente, producto, fecha,
+    cantidad, material, tintas, troquel y dimensiones.
     """
 )
 
